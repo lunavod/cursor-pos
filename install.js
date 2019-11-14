271d981f91d666f065a355b3fa9005d6e2c59bda
Егор Покровский <lunavod@bk.ru>
2019-11-14 17:52:37 +0300

    Update install.js

--- install.js
+++ install.js
@@ -2,7 +2,13 @@ const exec = require('child_process').exec;
 const platform = require('os').platform
 
 let moduleName
-if (platform=="linux") moduleName = 'lunavod/linux-cursor-pos'
-if (platform=="win32") moduleName = 'lunavod/windows-cursor-pos'
+
+if (platform=="win32") {
+  moduleName = 'lunavod/windows-cursor-pos'
+} else {
+  moduleName = 'lunavod/linux-cursor-pos'
+}
+
+console.log(`Installing ${moduleName}...`)
 
 exec(`npm i ${moduleName}`).stdout.pipe(process.stdout);
